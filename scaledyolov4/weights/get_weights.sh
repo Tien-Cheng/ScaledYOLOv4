d9420e432a5aaca9ff50a9c5857aa9f251828126
yhsmiley <46747906+yhsmiley@users.noreply.github.com>
2021-09-02 21:53:51 +0800

    download less models by default

--- scaledyolov4/weights/get_weights.sh
+++ scaledyolov4/weights/get_weights.sh
@@ -9,8 +9,8 @@ fi
 
 # download weights saved as state_dict
 gdown -O yolov4l-mish_-state.pt https://drive.google.com/uc?id=1VIkLSogNsrKdUx3qpxvj-mUKQMtGnifk
-gdown -O yolov4-p5-state.pt https://drive.google.com/uc?id=1J9CLEucAzQnnVucJfAmmhysPyK03A83N
+# gdown -O yolov4-p5-state.pt https://drive.google.com/uc?id=1J9CLEucAzQnnVucJfAmmhysPyK03A83N
 gdown -O yolov4-p5_-state.pt https://drive.google.com/uc?id=1r2rdKxvnXj8mYVF0Ku-U6lAX-4JVllxf
-gdown -O yolov4-p6-state.pt https://drive.google.com/uc?id=1BFTErx0FfAmkTRIhWnI-k0NjqiuCyIsO
+# gdown -O yolov4-p6-state.pt https://drive.google.com/uc?id=1BFTErx0FfAmkTRIhWnI-k0NjqiuCyIsO
 gdown -O yolov4-p6_-state.pt https://drive.google.com/uc?id=1lixDNHPhbZ7ZpexIzVPrTo2BEMgCFSIy
 gdown -O yolov4-p7-state.pt https://drive.google.com/uc?id=1c8YIBod6AjiJhxLBK0Cjwxfca7eo1_8J
